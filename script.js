db704a476c2b069ab2bd19748ecf2d1a9166868d
Carjardying <carolinejardy.pro@gmail.com>
2025-05-09 19:18:09 +0200

    j'ai essayé avec un push bc ajouter ligne tableau me ressortait push(). Ne fonctionne pas

--- script.js
+++ script.js
@@ -11,45 +11,28 @@ const calendrierDuCrayon = {
     juin: 1,
 };
 
-const crayon = `
+const crayon = [`
  /\\
 /__\\
-||||
-||||
-||||
-||||
-||||
-||||
-||||
-||||
-||||
-||||
-|__|
-|  |
-|__|`; // voir comment intégrer le caractère special "\"
+`]; // voir comment intégrer le caractère special "\"
 
-console.log(crayon);
+console.log("crayon simple", crayon);
 
 /* créer une fonction qui navigue dans l'objet et affiche plus ou moins de barres en fonction du mois.
 si c'était un tableau : boucle for => chaque fois que je passe à l'index ++, 1ligne de |||| en moins.
 traduire ça */
-let moisMars = calendrierDuCrayon.mars;
-console.log("appel hors fonction: " , moisMars);
+// const moisMars = calendrierDuCrayon.mars;
+// console.log("appel hors fonction: " , moisMars);
 
-function drawMyPen(calendrier){
-    const month = calendrier.mois; //vu que ça change
+function drawMyPen(calendrier, mois){
+    let months = calendrier[mois]; //vu dans l'exercice avec les candidates dragrace
 
-    for (let index = 0; index < calendrier; index++) {
-        console.log("boucle dans fonction for : "); // ne fonctionne pas
-        
+    for (let index = 0; index < months ; index++) {
+        crayon.push("||||"); //senser ajouter ça au crayon
     };
-
-    for (const element of calendrier) {
-        console.log("boucle for of : ",element); // epelle le nom du mois 
-    }
-
+    return crayon.join("\n") // pour joindre le push au reste dans une seule string
 }
 
 
 
-drawMyPen('mars'); //affiche mars et non le crayon
+console.log(drawMyPen('mars'));
