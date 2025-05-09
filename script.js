410878b6a172371f4b938387156e8590e4bb690b
Carjardying <carolinejardy.pro@gmail.com>
2025-05-09 12:41:03 +0200

    j'ai tenté des trucs mais je sais que rien ne fonctionne. Je vais manger

--- script.js
+++ script.js
@@ -11,8 +11,42 @@ const calendrierDuCrayon = {
     juin: 1,
 };
 
-function drawMyPen(crayon){
- for (const element of object) {
-    
+const crayon = `/
+/__
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+||||
+|__|
+|  |
+|__|`; // voir comment intégrer le caractère special "\"
+
+// console.log(crayon);
+
+/* créer une fonction qui navigue dans l'objet et affiche plus ou moins de barres en fonction du mois.
+si c'était un tableau : boucle for => chaque fois que je passe à l'index ++, 1ligne de |||| en moins.
+traduire ça */
+
+
+function drawMyPen(mois){
+ console.log(mois);
+ if (mois.key == 'mars') {
+    crayon -= `||||
+||||
+||||
+||||
+||||
+||||`
+console.log(crayon); //si placé or du if, affiche le crayon complet.
+ } //pour tenter. Ne fonctionnera surement pas.
+ 
  }
-}
+
+
+drawMyPen('mars'); //affiche mars et non le crayon
